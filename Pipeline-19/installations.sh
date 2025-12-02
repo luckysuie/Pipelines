f9a9266c9becfac46d4112a1704a2013f6cc7ea8
Lakshmi Narayana <139038292+luckysuie@users.noreply.github.com>
2025-12-02 06:39:41 +0530

    Update installations.sh

--- Pipeline-19/installations.sh
+++ Pipeline-19/installations.sh
@@ -42,7 +42,7 @@ sudo snap install --classic kubectl
 # Install argocd
 sudo curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo chmod +x /usr/local/bin/argocd
-argocd version
+argocd version --client
 
 # Prometheus (binary under /opt/prometheus)
 sudo apt update -y
